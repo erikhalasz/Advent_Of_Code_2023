b2500fc2a6e7e91fdf1b508c87375a8bd50cfb22
erikhalasz <erik.halasz02@gmail.com>
2023-12-04 22:28:00 +0100

    -szokoz

--- day_4/day4_2.py
+++ day_4/day4_2.py
@@ -10,7 +10,6 @@ lines = []
 for i in range(len(text)):
     lines.append(text[i].replace(f"Card {i+1}: ", "").split("|"))
 
-    x = 0
 
 instances = {}
 
@@ -32,47 +31,6 @@ for m in range(len(lines)):
 
 x = 0
 
-
-for key in instances:
-    x += instances[key]
-
-print(x)
-
-t2 = time.time()
-print(t2-t1)
-
-
-import time
-
-t1 = time.time()
-
-with open("C:/Users/Halász Erik/Documents/Advent_of_code_2023/day_4/file1.txt") as f:
-    text = f.read()
-    text = text.split("\n")
-
-cards = []
-
-for i in range(len(text)):
-    cards.append(list(map(int, text[i].replace(f"Card {i+1}: ", "").split("|")[0].split())))
-
-instances = {}
-
-for i in range(len(cards)):
-    instances[f"{i+1}"] = 1
-
-for m in range(len(cards)):
-    card = cards[m]
-    t = 0
-    for k in range(len(card)):
-        num = card[k]
-        l = set(cards[m+1].split())
-        if num in l:
-            t += 1
-    if t >= 1:
-        for y in range(1, t+1):
-            instances[f"{m + y + 1}"] += instances[f"{m+1}"]
-
-x = 0
 
 for key in instances:
     x += instances[key]
